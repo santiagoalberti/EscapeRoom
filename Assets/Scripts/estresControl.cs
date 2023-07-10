85bea23644d37419e4c6398121d29622c6090af5
riassogna <88850063+riassogna@users.noreply.github.com>
2023-07-09 23:55:29 -0300

    ahora si jaja

--- Assets/Scripts/estresControl.cs
+++ Assets/Scripts/estresControl.cs
@@ -35,7 +35,7 @@ public class estresControl : MonoBehaviour
     }
     public void reducirEnergia()
     {
-        energiaActual -= 0.80f*Time.deltaTime;
+        energiaActual -= 1.2f*Time.deltaTime;
         //Debug.Log("tengo" + energiaActual + "de" + energiaMaxima);
         barraVerde.fillAmount = energiaActual / energiaMaxima;
         if (energiaActual <= 50f)
